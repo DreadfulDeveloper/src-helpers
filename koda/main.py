b93bb6a13faeca39cdb268e52011ad95e21ead1b
Jacob Krogman <jacobkrogman92@gmail.com>
2017-09-19 23:12:57 -0400

    Add folder name declarations to top of file

--- koda/main.py
+++ koda/main.py
@@ -4,6 +4,9 @@ from glob import glob
 from mutagen.easyid3 import EasyID3
 import shutil
 
+UNPROCESSED_FOLEDER = "unprocessed"
+PROCESSED_FOLDER = "processed"
+
 CAMELOT_FOLDERS = ["1A - 1B", "2A - 2B", "3A - 3B", "4A - 4B", "5A - 5B", "6A - 6B", "7A - 7B", "8A - 8B", "9A - 9B", "10A - 10B", "11A - 11B", "12A - 12B"]
 
 def createOutputDirs():
@@ -21,16 +24,19 @@ def getDirContents(directory):
 def copyFileToFolder(src, dest):
     tags = EasyID3(src)
 
-    # filename = '{} - {} - {}.mp3'.format(tags['bpm'][0].encode("utf-8"), tags['title'][0].encode("utf-8"), tags['artist'][0].encode("utf-8"))
-    # destFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), "processed", dest, filename)
-    destFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), "processed", dest, filename)
+    #filename = '{} - {} - {}.mp3'.format(tags['bpm'][0].encode("utf-8"), tags['title'][0].encode("utf-8"), tags['artist'][0].encode("utf-8"))
+    #destFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), "processed", dest, filename)
+    destFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), PROCESSED_FOLER, dest)
+    print destFile
     shutil.copy2(src, destFile)
 
 def copyFilesToKeyFolders():
     createOutputDirs()
-    [copyFileToFolder(f, CAMELOT_FOLDERS[i]) for f in getDirContents("unprocessed") for y in f.split(' - ') for i in range(len(CAMELOT_FOLDERS)) for key in CAMELOT_FOLDERS[i].split(' - ') if key in y]
+
+    [copyFileToFolder(f, CAMELOT_FOLDERS[i]) for f in getDirContents(UNPROCESSED_FOLER) for y in f.split(' - ') for i in range(len(CAMELOT_FOLDERS)) for key in CAMELOT_FOLDERS[i].split(' - ') if key in y]
 
 def main():
+
     copyFilesToKeyFolders()
 
 if __name__== "__main__":
